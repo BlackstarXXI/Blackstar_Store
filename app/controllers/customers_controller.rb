1fc8b6e4b406e71ee09e949985b56e342cb0771d
Jeff Lopez <jeffersonlopez_21@yahoo.com>
2015-04-06 08:14:21 -0500

    added styles and modified customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,24 @@
 class CustomersController < ApplicationController
+
+  def create
+    
+    @customer = Customer.new(customer_params)
+    
+    @customer.save
+    
+    redirect_to orders_new_path(:customer => @customer)
+    
+  end
+  
+  def new
+    @customer = Customer.new
+  end
+
+  def customer_params
+    params.require(:customer).permit(:province_id, :first_name, :last_name, :street_address, :box_no, :city,
+                  :postal_code, :email, :phone_number)
+    
+  end
+  
+
 end
